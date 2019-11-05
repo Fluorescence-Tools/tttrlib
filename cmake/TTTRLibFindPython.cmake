b1476964ee8acf69c03de279d92545e3474c887d
tpeulen <thomas.otavio.peulen@gmail.com>
2019-11-05 15:15:24 -0800

    Build improvements
    * Ensure that python interpreter matches python libraries
    * fixed type in finding openmp

--- cmake/TTTRLibFindPython.cmake
+++ cmake/TTTRLibFindPython.cmake
@@ -1,25 +1,24 @@
 function(tttrlib_find_python)
-    FIND_PACKAGE(PythonInterp)
-    FIND_PACKAGE(PythonLibs)
+    FIND_PACKAGE(Python COMPONENTS Interpreter Development)
     execute_process(
-            COMMAND python -c "from __future__ import print_function; import numpy; print(numpy.get_include())"
+            COMMAND ${Python_EXECUTABLE} -c "from __future__ import print_function; import numpy; print(numpy.get_include())"
             OUTPUT_VARIABLE Python_NumPy_PATH
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     execute_process(
-            COMMAND python -c "from sysconfig import get_paths as gp; print(gp()['include'])"
+            COMMAND ${Python_EXECUTABLE} -c "from sysconfig import get_paths as gp; print(gp()['include'])"
             OUTPUT_VARIABLE Python_INCLUDE_DIR
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    execute_process(COMMAND python -c "import sys; print(sys.executable)"
+    execute_process(COMMAND ${Python_EXECUTABLE} -c "import sys; print(sys.executable)"
             RESULT_VARIABLE retval
             WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
             OUTPUT_VARIABLE python_full_path
             OUTPUT_STRIP_TRAILING_WHITESPACE)
     INCLUDE_DIRECTORIES(BEFORE ${Python_NumPy_PATH} ${Python_INCLUDE_DIR})
-    LINK_LIBRARIES(${PYTHON_LIBRARY})
+    LINK_LIBRARIES(${Python_LIBRARY})
     MESSAGE(STATUS "Python_NumPy_PATH='${Python_NumPy_PATH}'")
     MESSAGE(STATUS "Python_INCLUDE_DIR='${Python_INCLUDE_DIR}'")
-    MESSAGE(STATUS "PYTHON_LIBRARY='${PYTHON_LIBRARY}'")
+    MESSAGE(STATUS "Python_LIBRARY='${Python_LIBRARY}'")
 
 endfunction(tttrlib_find_python)
