ccae3555c2fb96e26e9e9b3dd294d02ae59fa3cc
tpeulen <thomas.otavio.peulen@gmail.com>
2019-11-05 23:09:00 -0800

    Work on builds app windows

--- cmake/TTTRLibFindPython.cmake
+++ cmake/TTTRLibFindPython.cmake
@@ -1,16 +1,16 @@
 function(tttrlib_find_python)
     FIND_PACKAGE(Python COMPONENTS Interpreter Development)
     execute_process(
-            COMMAND python -c "from __future__ import print_function; import numpy; print(numpy.get_include())"
+            COMMAND ${Python_EXECUTABLE} -c "from __future__ import print_function; import numpy; print(numpy.get_include())"
             OUTPUT_VARIABLE Python_NumPy_PATH
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     execute_process(
-            COMMAND python -c "from sysconfig import get_paths as gp; print(gp()['include'])"
+            COMMAND ${Python_EXECUTABLE} -c "from sysconfig import get_paths as gp; print(gp()['include'])"
             OUTPUT_VARIABLE Python_INCLUDE_DIR
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    execute_process(COMMAND python -c "import sys; print(sys.executable)"
+    execute_process(COMMAND ${Python_EXECUTABLE} -c "import sys; print(sys.executable)"
             RESULT_VARIABLE retval
             WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
             OUTPUT_VARIABLE python_full_path
