96496cbf8d7b8bbf1745d48a9c6182b9aa950813
tpeulen <thomas.otavio.peulen@gmail.com>
2019-11-06 00:20:41 -0800

    Work on builds app windows

--- cmake/TTTRLibFindPython.cmake
+++ cmake/TTTRLibFindPython.cmake
@@ -1,8 +1,24 @@
 function(tttrlib_find_python)
-    FIND_PACKAGE(Python3 COMPONENTS Interpreter Development NumPy)
-    INCLUDE_DIRECTORIES(BEFORE ${Python3_NumPy_INCLUDE_DIRS} ${Python3_INCLUDE_DIRS})
-    LINK_LIBRARIES(${Python3_LIBRARIES})
-    MESSAGE(STATUS "Python3_NumPy_INCLUDE_DIRS='${Python3_NumPy_INCLUDE_DIRS}'")
-    MESSAGE(STATUS "Python3_INCLUDE_DIRS='${Python3_INCLUDE_DIRS}'")
-    MESSAGE(STATUS "Python3_LIBRARIES='${Python3_LIBRARIES}'")
+    FIND_PACKAGE(Python COMPONENTS Interpreter Development)
+    execute_process(
+            COMMAND ${Python_EXECUTABLE} -c "from __future__ import print_function; import numpy; print(numpy.get_include())"
+            OUTPUT_VARIABLE Python_NumPy_PATH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+            COMMAND ${Python_EXECUTABLE} -c "from sysconfig import get_paths as gp; print(gp()['include'])"
+            OUTPUT_VARIABLE Python_INCLUDE_DIR
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(COMMAND ${Python_EXECUTABLE} -c "import sys; print(sys.executable)"
+            RESULT_VARIABLE retval
+            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+            OUTPUT_VARIABLE python_full_path
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+    INCLUDE_DIRECTORIES(BEFORE ${Python_NumPy_PATH} ${Python_INCLUDE_DIR})
+    LINK_LIBRARIES(${Python_LIBRARY})
+    MESSAGE(STATUS "Python_NumPy_PATH='${Python_NumPy_PATH}'")
+    MESSAGE(STATUS "Python_INCLUDE_DIR='${Python_INCLUDE_DIR}'")
+    MESSAGE(STATUS "Python_LIBRARY='${Python_LIBRARY}'")
+
 endfunction(tttrlib_find_python)
