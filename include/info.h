6f50f645b9f59d4996a54f92d1897e1e88f3346b
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2022-03-03 13:35:53 +0100

    Bump version number

--- include/info.h
+++ include/info.h
@@ -3,7 +3,7 @@
 
 #define RECORD_PHOTON               0
 #define RECORD_MARKER               1
-#define TTTRLIB_VERSION             "0.21.4"
-
+#define TTTRLIB_VERSION             "0.21.6"
+#define VERBOSE_TTTRLIB             1
 
 #endif //TTTRLIB_INFO_H
