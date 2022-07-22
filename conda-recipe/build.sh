39b404475263da1eac3ebe446da9e87c2c2c1eb6
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2022-07-22 00:12:47 +0000

    Update conda-recipe/meta.yaml, conda-recipe/build.sh

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -4,21 +4,21 @@ rm -r -f build
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 cd build
 
-cmake \
-  -DCMAKE_BUILD_TYPE=Release \
-  -G Ninja \
-  -DBUILD_PYTHON_INTERFACE=OFF \
-  -DCMAKE_INSTALL_LIBDIR=lib \
-  -DCMAKE_PREFIX_PATH=$PREFIX \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  ..
-ninja install -k 0
+# cmake \
+#   -DCMAKE_BUILD_TYPE=Release \
+#   -G Ninja \
+#   -DBUILD_PYTHON_INTERFACE=OFF \
+#   -DCMAKE_INSTALL_LIBDIR=lib \
+#   -DCMAKE_PREFIX_PATH=$PREFIX \
+#   -DCMAKE_INSTALL_PREFIX=$PREFIX \
+#   ..
+# ninja install -k 0
 
-#cmake \
-#  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-#  -DCMAKE_PREFIX_PATH=$PREFIX \
-#  -DBUILD_PYTHON_INTERFACE=OFF \
-#  -DCMAKE_BUILD_TYPE=Release \
-#  ..
-#make
-#make install
+cmake \
+ -DCMAKE_INSTALL_PREFIX=$PREFIX \
+ -DCMAKE_PREFIX_PATH=$PREFIX \
+ -DBUILD_PYTHON_INTERFACE=OFF \
+ -DCMAKE_BUILD_TYPE=Release \
+ ..
+make
+make install
