1314e8ff856ef98c5958670fc6a09be65aba9468
Thomas-Otavio Peulen <thomas.otavio.peulen@gmail.com>
2024-12-11 15:53:38 +0100

    Add submodule update

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,10 +1,12 @@
-mkdir b2 && cd b2
+# Update submodules
+git submodule update --recursive --init --remote
 
 if [[ "${target_platform}" == osx-* ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+mkdir b2 && cd b2
 cmake -S .. -B . \
   -DCMAKE_CXX_COMPILER="${CXX}" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
