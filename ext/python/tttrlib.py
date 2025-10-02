17c22b2fd99316cae0cbb347106500f04e7f8d86
tpeulen <thomas.peulen@posteo.de>
2025-10-02 11:31:01 +0200

    Refactor version handling in tttrlib to improve compatibility and error handling

--- ext/python/tttrlib.py
+++ ext/python/tttrlib.py
@@ -1,13 +1,12 @@
-import numpy as np
+from __future__ import annotations
 import sys
-import typing
 
 if sys.version_info[0] < 3:
     from importlib_metadata import version
-else:   
+else:
     from importlib.metadata import version
 
 try:
     __version__ = version(__package__ or __name__)
-except:
+except Exception:
     __version__ = "0.0.0"
