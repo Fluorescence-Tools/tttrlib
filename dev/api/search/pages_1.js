f2b1be7fd96c134598991d8ac5414a145e070022
tpeulen <tpeulen@users.noreply.github.com>
2026-03-09 08:30:11 +0000

    deploy: 35268b4946b1ef4dab6a9a72654622f4659d1f98

---
+++ dev/api/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['for_20tttrlib_0',['Documentation for tttrlib',['../md_README.html',1,'']]]
+];
---
+++ dev/api/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "cdfrt",
+  1: "cr",
+  2: "dft"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Pages"
+};
+
